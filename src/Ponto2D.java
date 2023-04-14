9c20ed6821d3df1f8d73755c97aa25513af5d623
danidbd <117097842+danidbd@users.noreply.github.com>
2023-04-14 14:12:08 -0300

    Add files via upload

---
+++ src/Ponto2D.java
@@ -0,0 +1,13 @@
+public class Ponto2D {
+    private double x;
+    private double y;
+
+    public Ponto2D(double x , double y){
+        this.x = x;
+        this.y = y;
+
+    }
+    public String toString() {
+        return "Ponto2D: (" + x + ", " + y + ")";
+    }
+}
